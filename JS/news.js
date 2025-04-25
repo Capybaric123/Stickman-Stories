a57b513c098559df9b8a12ed02944c2e566446c0
Capybaric123 <440026052@gapps.yrdsb.ca>
2025-04-25 14:17:26 -0400

    JS/news: update

--- JS/news.js
+++ JS/news.js
@@ -1,19 +1,7 @@
 //list of slides to be shown
-const content = [
-    'New script for announcement page!',
-    'Updated contribution guide!',
-    ''
-];
-
-let key = 0;
-const marquee = $('.marquee');
-marquee.on('animationstart', () => {
-    key = 0;
-    marquee.text(content[key]);
+$(function () {
+    $('.marquee').marquee({
+        duration: 1500,
+        duplicate: true
+    });
 });
-marquee.on('animationiteration', () => {
-    key++;
-    if(typeof content[key] === 'undefined') key = 0;
-    marquee.text(content[key]);
-});
-marquee.removeClass('paused');
